fe463a9dc0a5847105d23401e5106ca49110f2fe
Yang Zhang <yang@users.noreply.github.com>
2021-03-16 13:50:56 -0700

    Sync Plasmic project

---
+++ src/components/plasmic/minitwitter/icons/PlasmicIcon__Icon.tsx
@@ -0,0 +1,42 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export type IconIconProps = React.ComponentProps<"svg"> & {
+  title?: string;
+};
+
+export function IconIcon(props: IconIconProps) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      className={classNames(
+        "plasmic-default__svg",
+        className,
+        "MuiSvgIcon-root jss172"
+      )}
+      viewBox={"0 0 24 24"}
+      aria-hidden={"true"}
+      fill={"currentcolor"}
+      height={"1em"}
+      width={"1em"}
+      style={style}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={
+          "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+        }
+      ></path>
+    </svg>
+  );
+}
+
+export default IconIcon;
+/* prettier-ignore-end */
